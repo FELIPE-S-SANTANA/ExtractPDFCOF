464292bd9d9adc59a3c38ce997e459e312b7af04
FELIPE-S-SANTANA <42617830+FELIPE-S-SANTANA@users.noreply.github.com>
2018-08-22 18:14:11 -0300

    NEW Extract Cofins

--- ExtractPDFCofins.py
+++ ExtractPDFCofins.py
@@ -1,47 +1,40 @@
 import tabula
-import pandas
-
-
+import pandas as pd
 
 VALIDATOR = 'CONTATOR'
-HEAD_COFINS = ['Modelo', 'Série', 'Número', 'Dia da', 'Código', 'Descrição da Mercadoria', 'Código da Mercadoria',
-               'Código', 'UF', 'Unidade', 'Quantidade', 'Valor Unitário', 'Diferença', 'COFINS']
 
-description = ''
-dif = ''
-cofins = ''
-flag = 0
-
-
+HEAD_COFINS = ['Modelo', 'Série', 'Número_Nota', 'Emissão','Descrição',
+               'Cod_Mercadoria', 'CFOP', 'UF', 'Unidade', 'Quantidade', 'Valor', 'Diferença', 'COFINS']
+NEW_HEAD_COFINS = ['Modelo', 'Série', 'Número_Nota', 'Emissão','NCM', 'Descrição',
+               'Cod_Mercadoria', 'CFOP', 'UF', 'Unidade', 'Quantidade', 'Valor', 'Diferença', 'COFINS']
 def format_df(row):
-    global description, dif, cofins
-    if str(row['Descrição da Mercadoria']).count(VALIDATOR) and row['Modelo'] != '':
-        return row
-    elif str(row['Descrição da Mercadoria']).count(VALIDATOR) and row['Modelo'] == '':
-        description = row['Descrição da Mercadoria']
-        return row
-    elif not str(row['Descrição da Mercadoria']).count(VALIDATOR) and row['Modelo'] != '':
-        row['Descrição da Mercadoria'] = description+row['Descrição da Mercadoria']
-        row['Diferença'] = dif
-        row['COFINS'] = cofins
-        return row
-    elif row['Modelo'] == '' and row['Diferença'] != '':
-        dif = row['Diferença']
-        cofins = row['COFINS']
+    if row['Descrição'].count(VALIDATOR)==1:
+        index = int(row['index'])+1
+        x = df.loc[df['index'] == index, 'Descrição'].values[0]
+        df['NCM'] = row['Descrição'][:9]
+        row['Descrição'] = row['Descrição'][8:]+x
+
         return row
     else:
-        return row
+        return 'remove'
+
+df_result = pd.DataFrame()
+PATH = "C:\\Users\\felipe.santana\\desktop\\Jobs\\Thulio\\felipe\\Laudo\\COFINS\\2010 COFINS - 10283723413201666_05673_07166_DOCUMENTOSDIVERSOS-OUTROS.PDF"
+file = tabula.read_pdf(PATH, output_format='dataframe', encoding='ANSI', java_options=None, pages='all').fillna('')
+for i in file:
+    df = i
+    df.columns = HEAD_COFINS
+    df = df.drop(index=[0,1])
+    df['Descrição'] = df['Descrição'].astype(str)
+
+    df = df.reset_index()
+
+    df = df.apply(lambda row: format_df(row), axis=1)
+    df = df[~df.Descrição.str.contains("remove") == True]
+    df = df[NEW_HEAD_COFINS]
+    df_result = pd.concat(df_result,df)
 
 
-file = "C:\\Users\\felipe.santana\\desktop\\Jobs\\Thulio\\felipe\\Laudo\\COFINS\\2009 COFINS - 10283723413201666_05189_05672_DOCUMENTOSDIVERSOS-OUTROS.PDF"
-print('Reading File')
-df = tabula.read_pdf(file, output_format='dataframe', encoding='ANSI', java_options=None).fillna('') #pages='all'
-print('Formating File')
-# df = df.apply(lambda row: format_df(row), axis=1)
-# df = df.loc[(df['Modelo'] != '') & (df['Modelo'] != 'Modelo')]
-
-#
-# write = pandas.ExcelWriter('C:\\Users\\felipe.santana\\desktop\\Jobs\\Thulio\\felipe\\NEW\\NEW\\10283723413201666_05189_05672_DOCUMENTOSDIVERSOS-OUTROS.xlsx')
-# df.to_excel(write,'Teste', index=False)
-# write.save()
-
+write = pandas.ExcelWriter('C:\\Users\\felipe.santana\\desktop\\Jobs\\Thulio\\felipe\\NEW\\NEW\\2010 COFINS - 10283723413201666_05673_07166_DOCUMENTOSDIVERSOS-OUTROS.xlsx')
+df.to_excel(write,'Teste', index=False)
+write.save()
